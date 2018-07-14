f689f664fb19fe89d6a8a3a2a23b440d28a3ef98
hunghung166 <hungnvd00436@fpt.edu.vn>
2018-07-14 15:01:44 +0700

    nopbaitap

---
+++ EXAM_RESTServiceCRUD/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace EXAM_RESTServiceCRUD
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int EmployeeId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public decimal Salary { get; set; }
+
+        [DataMember]
+        public string Department { get; set; }
+    }
+
+    public interface IEmployeeRepository
+    {
+        List<Employee> GetAllEmployee();
+        Employee GetEmployeeByDepartment(string department);
+        Employee AddNewEmployee(Employee item);
+    }
+
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> employees = new List<Employee>();
+        private int counter = 1;
+
+        public EmployeeRepository()
+        {
+            AddNewEmployee(new Employee { EmployeeId = 1, Name = "Hung", Salary = 100000000, Department = "kinhdoanh"});
+            AddNewEmployee(new Employee { EmployeeId = 2, Name = "Tung", Salary = 100000000, Department = "laptrinh"});
+            AddNewEmployee(new Employee { EmployeeId = 3, Name = "Danh", Salary = 100000000, Department = "ketoan"});
+
+        }
+
+        public Employee AddNewEmployee(Employee newEmployee)
+        {
+            if (newEmployee == null)
+                throw new ArgumentNullException("newEmployee");
+            newEmployee.EmployeeId = counter++;
+            employees.Add(newEmployee);
+            return newEmployee;
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployeeByDepartment(string department)
+        {
+            return employees.Find(b => b.Department == department);
+        }
+    }
+}
---
+++ EXAM_RESTServiceCRUD/EmployeeService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EXAM_RESTServiceCRUD
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+        static IEmployeeRepository repository = new EmployeeRepository();
+
+        public string AddEmployee(Employee employee, string id)
+        {
+            Employee newEmployee = repository.AddNewEmployee(employee);
+            return "id=" + newEmployee.EmployeeId;
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public Employee GetEmployeeByDepartment(string department)
+        {
+            return repository.GetEmployeeByDepartment(department);
+        }
+
+        public List<Employee> GetEmployeeList()
+        {
+            return repository.GetAllEmployee();
+        }
+    }
+}
